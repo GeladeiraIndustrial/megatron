68ba62547289b4b2153278a6b1c4f6c61a24e4cc
GeladeiraIndustrial <130378197+GeladeiraIndustrial@users.noreply.github.com>
2024-08-27 10:58:48 -0300

    aaaa

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 const caixaPrincipal = document.querySelector(".caixa-principal"); 
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixaalternativas");
-const caixaResultado = document.querySelector(".caixa-resultados");
-const caixaTexto = document.querySelector(".caixa-texto");
+const caixaAlternativas = document.querySelector(".caixalternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaTexto = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["alternativa1","alternativa2","alternativa3"]
+
+    },
+    {
+
+    },
