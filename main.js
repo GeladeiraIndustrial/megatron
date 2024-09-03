8af27b37bebb8dced7b6f47521ae28c5fc59668c
GeladeiraIndustrial <130378197+GeladeiraIndustrial@users.noreply.github.com>
2024-09-03 11:10:19 -0300

    kkkkk

--- main.js
+++ main.js
@@ -26,3 +26,14 @@ const perguntas = [
 
 ];
 
+let atual = 0
+let perguntaAtual;
+
+function mostraPergunta(){
+perguntaAtual = perguntas[atual];
+
+caixaPergunta.textContent = perguntaAtual.enunciado;
+
+}
+
+mostraPergunta();
